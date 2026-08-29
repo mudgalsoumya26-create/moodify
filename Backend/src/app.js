5e31806f1641447b9649d839b177988a2d02dfca
mudgalsoumya26-create <mudgalsoumya26@gmail.com>
2026-08-29 22:05:03 +0530

    final touches

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:'https://frontend-soumyas26.vercel.app',
+    origin:'https://sonora-soumyas26.vercel.app',
     credentials:true
 }))
 app.use('/api/auth',authRouter)
